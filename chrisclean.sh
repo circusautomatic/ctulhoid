3724231d5c3dc1a957130e843a7d19d2c61ef1d1
cpyarger <cpyarger@gmail.com>
2017-08-09 18:55:16 -0700

    cleanup git repo and add chris clean script, and remove temp files

---
+++ chrisclean.sh
@@ -0,0 +1,13 @@
+# For PCBs designed using KiCad: http://www.kicad-pcb.org/
+
+# Temporary files
+git rm -r *.000
+git rm -r *.bak
+git rm -r *.bck
+git rm -r *.kicad_pcb-bak
+git rm -r  *~ _autosave-*
+git rm -r  *.tmp
+git rm -r   __MACOSX*
+git rm -r  *.net
+git rm -r  *.dsn
+git rm -r  *.ses
